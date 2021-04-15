ac979d976dc9de01431b3503b8eb0da7b7a38c65
Suresh Nagar <nagars@vmware.com>
2021-04-15 16:21:37 -0700

    Updating docs folder for demo. Moving away from gh-pages for publishing demo

--- dist/ngx-mousetrap/lib/ngx-mousetrap.module.d.ts
+++ dist/ngx-mousetrap/lib/ngx-mousetrap.module.d.ts
@@ -1,4 +1,10 @@
 import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './ngx-mousetrap.directive';
 export declare class NgxMousetrapModule {
     static forRoot(): ModuleWithProviders<NgxMousetrapModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<NgxMousetrapModule, [typeof ɵngcc1.NgxMousetrapDirective], never, [typeof ɵngcc1.NgxMousetrapDirective]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<NgxMousetrapModule>;
 }
+
+//# sourceMappingURL=ngx-mousetrap.module.d.ts.map
